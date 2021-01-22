478d25e883719fb357b246f4282377df076ff565
PorterOBrien2021 <824085@mystma.org>
2021-01-22 15:45:44 +0000

    factorsum

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#WRITE YOUR CODE IN THIS FILE
+def factOrSum(x, y):
+    if y == "factorial":
+        total = 1
+
+        for i in range(0, x +1):
+            total = total * i
+
+        return total
+
+    else:
+        total = 0
+
+        for i in range(0, x + 1):
+            total = total + i
+
+        return total
+
+print (factOrSum(5, sum))
